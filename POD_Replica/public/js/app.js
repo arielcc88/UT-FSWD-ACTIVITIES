3da63f18a6eef3c1e90c003e990c522fd3cd1ff9
Ariel Cuesta <arielcc88@gmail.com>
2020-08-05 08:16:05 -0500

    copy of pod app for separate work.

---
+++ POD_Replica/public/js/app.js
@@ -0,0 +1,50 @@
+$(document).foundation()
+
+//lessons content filtering
+// shows and hides filtered items
+$(".filter-simple-button").click(function() {
+    var value = $(this).attr('data-filter');
+    if(value === "all") {
+      $('.filter-simple-item').show('1000');
+    } else {
+      $(".filter-simple-item").not('.'+value).hide('3000');
+      $('.filter-simple-item').filter('.'+value).show('3000');
+    }
+  });
+  
+  // changes active class on filter buttons
+  $('.filter-simple-button').click(function () {
+    $(this).siblings().removeClass('is-active');
+    $(this).addClass('is-active');
+  });
+  
+  //Log in button
+  $("#signin-Login").on("click", function(){
+    let userId = $("#userID-Login").val()
+    let pwd = $("#pwd-Login").val()
+    if (emailIsValid(userId) && pwd.length >= 8 ){
+      
+        console.log("Am here")
+
+        console.log(userId)
+    } 
+      else if (userId == ("") || pwd == ("")){
+      console.log("A valid email & pwd required")
+    } else if (!(emailIsValid(userId))){
+      console.log("Please enter a valid email address")
+    }
+    
+  })
+
+  //function to register new account
+  
+
+  
+  
+
+
+  //function to validate email
+  function emailIsValid (email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+  }
+  
---
+++ sequelize_practice/app.js
@@ -0,0 +1,31 @@
+// *****************************************************************************
+// Server.js - App Starting point. Initializes Node JS server
+//
+// ******************************************************************************
+// *** Importing Dependencies
+// =============================================================
+const express = require("express");
+
+// Instantiating Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//setting data parsing middlewares --JSON-- with Express
+app.use(express.urlencoded( { extended: true } ));
+app.use(express.json()); //request body parser
+
+
+//Setting static directory = public
+// app.use(express.static("public"));
+
+
+// Routes
+// =============================================================
+
+
+// Server Initiated - Listening for incoming requests
+// =============================================================
+app.listen(PORT, () => {
+    console.log(`SERVER ON => App listening on PORT ${PORT}`)
+});
